07d74129402e2b75a7f168e34c86b5a57624c326
cromod <florent.sarembaud@gmail.com>
2020-08-30 00:26:45 +0200

    Add support features

--- app/src/main/java/fr/cromod/easyinterest/MainActivity.kt
+++ app/src/main/java/fr/cromod/easyinterest/MainActivity.kt
@@ -1,5 +1,8 @@
 package fr.cromod.easyinterest
 
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
@@ -31,8 +34,10 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
                 .setAction("Action", null).show()
         }
 
-        val toggle = ActionBarDrawerToggle(this, drawer_layout, toolbar,
-            R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        val toggle = ActionBarDrawerToggle(
+            this, drawer_layout, toolbar,
+            R.string.navigation_drawer_open, R.string.navigation_drawer_close
+        )
         drawer_layout.addDrawerListener(toggle)
         toggle.syncState()
         nav_view.setNavigationItemSelectedListener(this)
@@ -71,10 +76,25 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
     {
         when(item.itemId)
         {
-            R.id.compound_interest -> loadFragment(CompoundInterestFragment.newInstance(), R.string.compound_interest)
-            R.id.property_loan -> loadFragment(PropertyLoanFragment.newInstance(), R.string.property_loan)
-            R.id.loan_prepayment -> loadFragment(LoanPrepaymentFragment.newInstance(), R.string.loan_prepayment)
-            R.id.loan_repurchase -> loadFragment(LoanRepurchaseFragment.newInstance(), R.string.loan_repurchase)
+            R.id.nav_compound_interest -> loadFragment(
+                CompoundInterestFragment.newInstance(),
+                R.string.compound_interest
+            )
+            R.id.nav_property_loan -> loadFragment(
+                PropertyLoanFragment.newInstance(),
+                R.string.property_loan
+            )
+            R.id.nav_loan_prepayment -> loadFragment(
+                LoanPrepaymentFragment.newInstance(),
+                R.string.loan_prepayment
+            )
+            R.id.nav_loan_repurchase -> loadFragment(
+                LoanRepurchaseFragment.newInstance(),
+                R.string.loan_repurchase
+            )
+            R.id.nav_rate -> rateApp()
+            R.id.nav_share -> shareApp()
+            R.id.nav_email_us -> sendEmail()
         }
         drawer_layout.closeDrawer(GravityCompat.START)
         return true
@@ -88,6 +108,38 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         {
             setTitle(getString(title))
             supportFragmentManager.beginTransaction().replace(fragment_to_display.id, fragment).commit()
+        }
+    }
+
+    private fun shareApp()
+    {
+        val shareIntent = Intent(Intent.ACTION_SEND)
+        shareIntent.type = "text/plain"
+        shareIntent.putExtra(Intent.EXTRA_TEXT, getString(R.string.share_this_app))
+        startActivity(Intent.createChooser(shareIntent, getString(R.string.share_to)))
+    }
+
+    private fun sendEmail()
+    {
+        val mailIntent = Intent(Intent.ACTION_SENDTO)
+        mailIntent.data = Uri.parse("mailto:" + getString(R.string.app_email))
+        startActivity(mailIntent)
+    }
+
+    private fun rateApp()
+    {
+        val rateIntent = Intent(Intent.ACTION_VIEW)
+        val appPackageName = "fr.cromod.easyinterest"
+
+        try
+        {
+            rateIntent.data = Uri.parse("market://details?id=$appPackageName")
+            startActivity(rateIntent)
+        }
+        catch (e: ActivityNotFoundException)
+        {
+            rateIntent.data = Uri.parse("http://play.google.com/store/apps/details?id=$appPackageName")
+            startActivity(rateIntent)
         }
     }
 }
